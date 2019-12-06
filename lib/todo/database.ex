3dd2c6a27b446f56c8de2e0571339b24c5860a59
Lu Zhao <louie.zhao@gmail.com>
2019-12-06 16:29:18 +0800

    introduce database worker pooling in database

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -8,33 +8,35 @@ defmodule Todo.Database do
   def fetch(name), do: GenServer.call(__MODULE__, {:fetch, name})
   def store(todo_list), do: GenServer.cast(__MODULE__, {:store, todo_list})
 
-  @dir "./persist"
+  @worker_count 3
 
   @impl GenServer
   def init(_) do
-    File.mkdir_p(@dir)
-    {:ok, nil}
+    {:ok, 1..@worker_count |> Enum.map(&create_worker/1)}
   end
 
   @impl GenServer
   def handle_call({:fetch, name}, _, state) do
     todo_list =
-      case File.read(file_path(name)) do
-        {:ok, data} -> :erlang.binary_to_term(data)
-        _ -> nil
-      end
+      choose_worker(state, name)
+      |> Todo.DatabaseWorker.fetch(name)
 
     {:reply, todo_list, state}
   end
 
   @impl GenServer
   def handle_cast({:store, todo_list}, state) do
-    todo_list.name
-    |> file_path
-    |> File.write!(:erlang.term_to_binary(todo_list))
-
+    choose_worker(state, todo_list.name) |> Todo.DatabaseWorker.store(todo_list)
     {:noreply, state}
   end
 
-  defp file_path(name), do: Path.join(@dir, name)
+  defp create_worker(_) do
+    {:ok, worker} = Todo.DatabaseWorker.start()
+    worker
+  end
+
+  defp choose_worker(state, name) do
+    state
+    |> Enum.at(:erlang.phash2(name, @worker_count))
+  end
 end
---
+++ lib/todo/database_worker.ex
@@ -0,0 +1,40 @@
+defmodule Todo.DatabaseWorker do
+  @moduledoc """
+  persistent storage for todo list
+  """
+  use GenServer
+
+  def start, do: GenServer.start(__MODULE__, nil)
+  def fetch(server, name), do: GenServer.call(server, {:fetch, name})
+  def store(server, todo_list), do: GenServer.cast(server, {:store, todo_list})
+
+  @dir "./persist"
+
+  @impl GenServer
+  def init(_) do
+    File.mkdir_p(@dir)
+    {:ok, nil}
+  end
+
+  @impl GenServer
+  def handle_call({:fetch, name}, _, state) do
+    todo_list =
+      case File.read(file_path(name)) do
+        {:ok, data} -> :erlang.binary_to_term(data)
+        _ -> nil
+      end
+
+    {:reply, todo_list, state}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, todo_list}, state) do
+    todo_list.name
+    |> file_path
+    |> File.write!(:erlang.term_to_binary(todo_list))
+
+    {:noreply, state}
+  end
+
+  defp file_path(name), do: Path.join(@dir, name)
+end
